06a4e56db95b2582aeeb771350338d69d8ba67e9
Vincent Mikl <miklv75@googlemail.com>
2025-10-22 12:33:57 +0000

    add new packages

--- ws_moveit/src/mecademic_bringup/setup.py
+++ ws_moveit/src/mecademic_bringup/setup.py
@@ -1,37 +1,38 @@
 from setuptools import setup, find_packages
 from glob import glob
-import os
 
 package_name = "mecademic_bringup"
 
 setup(
     name=package_name,
     version="0.1.0",
-    packages=find_packages(exclude=["test"]),
+    # Falls du keine Python-Module außer __init__.py hast, ist das ok.
+    packages=find_packages(include=[package_name, f"{package_name}.*"]),
     data_files=[
-        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
-        ("share/" + package_name, ["package.xml"]),
-        ("share/" + package_name + "/launch", glob("launch/*.launch.py")),
-        ("share/" + package_name + "/config", glob("config/*.yaml") + glob("config/*.rviz")),
-        ("share/" + package_name + "/resource/environment", glob("resource/environment/*.stl")),
-        ("share/" + package_name + "/resource/substrate_mounts", glob("resource/substrate_mounts/*.stl")),
-        ("share/" + package_name + "/resource/substrates", glob("resource/substrates/*.stl")),
-        ("share/" + package_name + "/resource/tools", glob("resource/tools/*.stl")),
-        ("share/" + package_name + "/spray_paths", glob("spray_paths/*.yaml")),
+        # ROS 2 package index
+        ("share/ament_index/resource_index/packages", [f"resource/{package_name}"]),
+        # package.xml
+        (f"share/{package_name}", ["package.xml"]),
+        # Launch files
+        (f"share/{package_name}/launch", glob("launch/*.launch.py")),
+        # Zentrale Configs
+        (f"share/{package_name}/config", glob("config/*.yaml") + glob("config/*.rviz")),
+        # Zusätzliche YAMLs (z.B. Pfade)
+        (f"share/{package_name}/spray_paths", glob("spray_paths/*.yaml")),
+        # Ressourcen/Meshes
+        (f"share/{package_name}/resource/environment", glob("resource/environment/*.stl")),
+        (f"share/{package_name}/resource/substrate_mounts", glob("resource/substrate_mounts/*.stl")),
+        (f"share/{package_name}/resource/substrates", glob("resource/substrates/*.stl")),
+        (f"share/{package_name}/resource/tools", glob("resource/tools/*.stl")),
     ],
     install_requires=["setuptools"],
     zip_safe=True,
     maintainer="miklv",
     maintainer_email="miklv75@googlemail.com",
-    description="Mecademic Robot Bringup: TF frames, scenes, tools, spray paths.",
+    description="Mecademic Bringup: Launch + zentrale Configs/Ressourcen.",
     license="Apache-2.0",
+    # Keine console_scripts mehr hier – die liegen jetzt in mecademic_nodes_py / _cpp
     entry_points={
-        'console_scripts': [
-            'motion_manager = mecademic_bringup.motion_manager:main',
-            'scene_manager = mecademic_bringup.scene_manager:main',
-            'poses_manager = mecademic_bringup.poses_manager:main',
-            'tool_manager = mecademic_bringup.tools.tool_manager:main',
-            'spray_path_manager = mecademic_bringup.spray_path_manager:main',
-        ],
+        "console_scripts": [],
     },
 )
